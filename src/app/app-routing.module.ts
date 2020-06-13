115c331296fa20a3ac59eae777eb3900f9ab0be5
junglewilly <guillaumec.dev@gmail.com>
2020-06-13 11:13:29 +0200

    create app-container and put header footer and home in it + put some html and css in home template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppContainerComponent } from './app-container/app-container.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppContainerComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './app-container/header/header.component';
+import { FooterComponent } from './app-container/footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './app-container/home/home.component';
+import { AppContainerComponent } from './app-container/app-container.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { ProjectsComponent } from './projects/projects.component';
     FooterComponent,
     AboutComponent,
     ProjectsComponent,
+    HomeComponent,
+    AppContainerComponent,
   ],
   imports: [
     BrowserModule,
