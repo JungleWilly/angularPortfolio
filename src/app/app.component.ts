f97ffbb08eb98a08c3a50f57679a22ae1cf6fa8d
junglewilly <guillaumec.dev@gmail.com>
2020-07-24 00:23:04 +0200

    try to link firebase on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ import { RouterOutlet } from '@angular/router';
 import { MatSidenav } from '@angular/material/sidenav';
 import { SidenavService } from './shared/service/sidenav.service';
 
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -47,6 +49,20 @@ export class AppComponent implements OnInit {
     private themeService: ThemeService,
     private sidenavService: SidenavService
   ) {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyBIFhRHX_v0c9HbXuGPnB_1mlXSNQD3aAg',
+      authDomain: 'guillaumeportfolio-c398d.firebaseapp.com',
+      databaseURL: 'https://guillaumeportfolio-c398d.firebaseio.com',
+      projectId: 'guillaumeportfolio-c398d',
+      storageBucket: 'guillaumeportfolio-c398d.appspot.com',
+      messagingSenderId: '127419266272',
+      appId: '1:127419266272:web:35cf95da0d079e94b18c85',
+      measurementId: 'G-M9ZXFLLVHT',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
     this.themeService.load();
   }
 
